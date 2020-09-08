2c3a283442dbcaa12b1cea6c9f1ff6b4643c3882
pony <916837189@qq.com>
2020-09-09 00:34:29 +0800

    算法开始

---
+++ src/alg/ListNode.java
@@ -0,0 +1,14 @@
+package alg;
+
+public class ListNode {
+     ListNode head;
+     ListNode next;
+    ListNode(){
+
+    }
+    ListNode(ListNode head,ListNode next){
+        this.head = head;
+        this.next = next;
+    }
+
+}
